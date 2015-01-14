107308afb674f12ee07d21833112c5f21cc59f18
Didip Kerabat <didip@newrelic.com>
2015-01-13 19:32:12 -0800

    Writer: Basic HTTP writer.

---
+++ writers/http.go
@@ -0,0 +1,37 @@
+package writers
+
+import (
+	"bytes"
+	"github.com/ddliu/go-httpclient"
+)
+
+// NewHttp is Http constructor.
+func NewHttp() *Http {
+	h := &Http{}
+	return h
+}
+
+// Http is a writer that simply serialize all readers data to Http.
+type Http struct {
+	Base
+	Url     string
+	Method  string
+	Headers map[string]string
+}
+
+func (h *Http) Run() error {
+	h.Data = h.GetReadersData()
+	inJson, err := h.ToJson()
+
+	if err != nil {
+		return err
+	}
+
+	client := httpclient.Defaults(httpclient.Map{
+		httpclient.OPT_USERAGENT: "ResourceD/1.0",
+		"Accept-Language":        "en-us",
+	})
+
+	_, err = client.Do(h.Method, h.Url, h.Headers, bytes.NewReader(inJson))
+	return err
+}
---
+++ writers/http_test.go
@@ -0,0 +1,53 @@
+package writers
+
+import (
+	"testing"
+)
+
+func jsonReadersDataForHttpTest() []byte {
+	jsonData := `{
+    "Data": {
+        "LoadAvg15m": 1.59375,
+        "LoadAvg1m": 1.5537109375,
+        "LoadAvg5m": 1.68798828125
+    },
+    "GoStruct": "LoadAvg",
+    "Hostname": "example.com",
+    "Interval": "1s",
+    "Path": "/load-avg",
+    "Tags": [ ],
+    "UnixNano": 1420607791403576000
+}`
+	return []byte(jsonData)
+}
+
+func TestNewHttpSetReadersData(t *testing.T) {
+	n := NewHttp()
+
+	readersData := make(map[string][]byte)
+	readersData["/load-avg"] = jsonReadersDataForHttpTest()
+
+	n.SetReadersData(readersData)
+
+	key := "/load-avg"
+	_, ok := n.GetReadersData()[key]
+	if !ok {
+		t.Errorf("Key does not exist. Key: %v, Data: %v", key, n.GetReadersData())
+	}
+}
+
+func TestNewHttpRun(t *testing.T) {
+	n := NewHttp()
+
+	readersData := make(map[string][]byte)
+	readersData["/load-avg"] = jsonReadersDataForHttpTest()
+
+	n.SetReadersData(readersData)
+	n.Url = "http://example.com/"
+	n.Method = "POST"
+
+	err := n.Run()
+	if err != nil {
+		t.Errorf("Run() should never fail. Error: %v", err)
+	}
+}
