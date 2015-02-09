2ff80a00b05a6844b2be1faecd71544642c8c32a
Didip Kerabat <didipk@gmail.com>
2015-02-08 18:37:41 -0800

    First pass on creating ResourcedMaster writer.

---
+++ writers/resouced_master_test.go
@@ -0,0 +1,54 @@
+package writers
+
+import (
+	"testing"
+)
+
+func jsonReadersDataForResourcedMasterTest() []byte {
+	jsonData := `{
+    "Data": {
+        "LoadAvg15m": 1.59375,
+        "LoadAvg1m": 1.5537109375,
+        "LoadAvg5m": 1.68798828125
+    },
+    "GoStruct": "LoadAvg",
+    "Hostname": "example.com",
+    "Interval": "1s",
+    "Path": "/load-avg",
+    "Tags": [ ],
+    "UnixNano": 1420607791403576000
+}`
+	return []byte(jsonData)
+}
+
+func newWriterForResourcedMasterTest() *ResourcedMaster {
+	n := NewResourcedMaster()
+
+	readersData := make(map[string][]byte)
+	readersData["/load-avg"] = jsonReadersDataForResourcedMasterTest()
+
+	n.SetReadersData(readersData)
+
+	return n
+}
+
+func TestNewResourcedMasterSetReadersData(t *testing.T) {
+	n := newWriterForResourcedMasterTest()
+
+	key := "/load-avg"
+	_, ok := n.GetReadersData()[key]
+	if !ok {
+		t.Errorf("Key does not exist. Key: %v, Data: %v", key, n.GetReadersData())
+	}
+}
+
+func TestNewResourcedMasterRun(t *testing.T) {
+	n := newWriterForResourcedMasterTest()
+	n.Url = "http://localhost:55655/"
+	n.Method = "POST"
+
+	err := n.Run()
+	if err != nil {
+		t.Errorf("Run() should never fail. Error: %v", err)
+	}
+}
