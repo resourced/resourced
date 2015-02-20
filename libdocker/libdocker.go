d9355183617b65984380aad9b3310c9452d4eb8d
Didip Kerabat <didipk@gmail.com>
2015-02-19 18:51:30 -0800

    Added functions to get all container and images data.

--- libdocker/libdocker.go
+++ libdocker/libdocker.go
@@ -5,6 +5,7 @@ import (
 	dockerclient "github.com/fsouza/go-dockerclient"
 	"os"
 	"path"
+	"sync"
 )
 
 // DockerClient returns dockerclient.Client which handles Docker connection.
@@ -29,10 +30,101 @@ func DockerClient() (*dockerclient.Client, error) {
 // AllContainers is a convenience function to fetch a slice of all containers data.
 func AllContainers() ([]dockerclient.APIContainers, error) {
 	client, err := DockerClient()
-
 	if err != nil {
 		return nil, err
 	}
 
 	return client.ListContainers(dockerclient.ListContainersOptions{})
+}
+
+// AllInspectedContainers is a convenience function to fetch a slice of all inspected containers data.
+func AllInspectedContainers() ([]*dockerclient.Container, error) {
+	client, err := DockerClient()
+	if err != nil {
+		return nil, err
+	}
+
+	shortDescContainers, err := client.ListContainers(dockerclient.ListContainersOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	containersChan := make(chan *dockerclient.Container)
+	var wg sync.WaitGroup
+
+	for _, shortDescContainer := range shortDescContainers {
+		wg.Add(1)
+
+		go func(shortDescContainer dockerclient.APIContainers) {
+			defer wg.Done()
+
+			fullDescContainer, err := client.InspectContainer(shortDescContainer.ID)
+			if err == nil && fullDescContainer != nil {
+				containersChan <- fullDescContainer
+			}
+		}(shortDescContainer)
+	}
+
+	containers := make([]*dockerclient.Container, 0)
+
+	go func() {
+		for container := range containersChan {
+			containers = append(containers, container)
+		}
+	}()
+
+	wg.Wait()
+
+	return containers, nil
+}
+
+// AllImages is a convenience function to fetch a slice of all images data.
+func AllImages() ([]dockerclient.APIImages, error) {
+	client, err := DockerClient()
+	if err != nil {
+		return nil, err
+	}
+
+	return client.ListImages(dockerclient.ListImagesOptions{})
+}
+
+// AllInspectedImages is a convenience function to fetch a slice of all inspected images data.
+func AllInspectedImages() ([]*dockerclient.Image, error) {
+	client, err := DockerClient()
+	if err != nil {
+		return nil, err
+	}
+
+	shortDescImages, err := client.ListImages(dockerclient.ListImagesOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	imagesChan := make(chan *dockerclient.Image)
+	var wg sync.WaitGroup
+
+	for _, shortDescImage := range shortDescImages {
+		wg.Add(1)
+
+		go func(shortDescImage dockerclient.APIImages) {
+			defer wg.Done()
+
+			fullDescImage, err := client.InspectImage(shortDescImage.ID)
+			if err == nil && fullDescImage != nil {
+				imagesChan <- fullDescImage
+			}
+		}(shortDescImage)
+	}
+
+	images := make([]*dockerclient.Image, 0)
+
+	go func() {
+		for image := range imagesChan {
+			images = append(images, image)
+		}
+	}()
+
+	wg.Wait()
+
+	return images, nil
 }
--- libdocker/libdocker_test.go
+++ libdocker/libdocker_test.go
@@ -10,3 +10,24 @@ func TestAllContainers(t *testing.T) {
 		t.Errorf("Gettting Docker containers info should not fail. Error: %v", err)
 	}
 }
+
+func TestAllInspectedContainers(t *testing.T) {
+	_, err := AllInspectedContainers()
+	if err != nil {
+		t.Errorf("Gettting Docker containers info should not fail. Error: %v", err)
+	}
+}
+
+func TestAllImages(t *testing.T) {
+	_, err := AllImages()
+	if err != nil {
+		t.Errorf("Gettting Docker images info should not fail. Error: %v", err)
+	}
+}
+
+func TestAllInspectedImages(t *testing.T) {
+	_, err := AllInspectedImages()
+	if err != nil {
+		t.Errorf("Gettting Docker images info should not fail. Error: %v", err)
+	}
+}
