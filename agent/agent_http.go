716f0ce003fa90564dd72d0edb67c5b07fa901b7
Didip Kerabat <didipk@gmail.com>
2015-04-29 23:37:23 -0700

    remove pprof

--- agent/agent_http.go
+++ agent/agent_http.go
@@ -6,7 +6,6 @@ import (
 	"github.com/julienschmidt/httprouter"
 	resourced_config "github.com/resourced/resourced/config"
 	"net/http"
-	"net/http/pprof"
 	"strings"
 )
 
@@ -275,14 +274,6 @@ func (a *Agent) HttpRouter() *httprouter.Router {
 	for writerPath, writerHandler := range a.MapWritersGetHandlers() {
 		router.GET(writerPath, writerHandler)
 	}
-
-	// Profiler
-	router.GET("/debug/pprof/profile", func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		pprof.Profile(w, r)
-	})
-	router.GET("/debug/pprof/heap", func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		pprof.Index(w, r)
-	})
 
 	return router
 }
