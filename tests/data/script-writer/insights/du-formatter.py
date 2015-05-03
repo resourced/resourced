8270394876a83156118dd05895fa53b3869a3d98
Didip Kerabat <didipk@gmail.com>
2015-05-03 11:09:51 -0700

    Add tests to json-flattener.

--- tests/data/script-writer/insights/du-formatter.py
+++ tests/data/script-writer/insights/du-formatter.py
@@ -11,7 +11,6 @@ EVENT_TYPE = "ServerDiskUsage"
 def read():
     return json.loads(sys.stdin.read())
 
-
 def reformat_output(in_json):
     items = []
 
@@ -28,7 +27,6 @@ def reformat_output(in_json):
         items.append(du_data)
 
     return items
-
 
 def run_tests():
     in_string = '''{"Data":{"/":{"DeviceName":"/dev/disk0s2","Free":2.9958176768e+10,"InodesFree":7.314008e+06,"InodesTotal":3.8862822e+07,"InodesUsed":3.1548814e+07,"InodesUsedPercent":81.17993592951125,"Path":"/","Total":1.59182127104e+11,"Used":1.28961806336e+11,"UsedPercent":81.01525509314506},"/Volumes/Files":{"DeviceName":"/dev/disk1s12","Free":5.6172371968e+10,"InodesFree":1.3713958e+07,"InodesTotal":2.6148632e+07,"InodesUsed":1.2434674e+07,"InodesUsedPercent":47.55382231850599,"Path":"/Volumes/Files","Total":1.07104804864e+11,"Used":5.0932432896e+10,"UsedPercent":47.55382632989547},"/Volumes/Main":{"DeviceName":"/dev/disk1s10","Free":1.4542032896e+10,"InodesFree":3.550301e+06,"InodesTotal":5.1924052e+07,"InodesUsed":4.8373751e+07,"InodesUsedPercent":93.16251166222543,"Path":"/Volumes/Main","Total":2.12680925184e+11,"Used":1.98138892288e+11,"UsedPercent":93.1625119255904},"/Volumes/Vagrant":{"DeviceName":"/dev/disk2s1","Free":2.1991424e+07,"InodesFree":5369,"InodesTotal":25588,"InodesUsed":20219,"InodesUsedPercent":79.01750820697202,"Path":"/Volumes/Vagrant","Total":1.0481664e+08,"Used":8.2825216e+07,"UsedPercent":79.01914810472842},"/dev":{"DeviceName":"devfs","Free":0,"InodesFree":0,"InodesTotal":686,"InodesUsed":686,"InodesUsedPercent":100,"Path":"/dev","Total":202752,"Used":202752,"UsedPercent":100},"/home":{"DeviceName":"map auto_home","Free":0,"InodesFree":0,"InodesTotal":0,"InodesUsed":0,"Path":"/home","Total":0,"Used":0},"/net":{"DeviceName":"map -hosts","Free":0,"InodesFree":0,"InodesTotal":0,"InodesUsed":0,"Path":"/net","Total":0,"Used":0}},"GoStruct":"Du","Host":{"Name":"MacBook-Pro.local","Tags":[]},"Interval":"3s","Path":"/du","UnixNano":1430674834475700306}'''
--- tests/data/script-writer/json-flattener.py
+++ tests/data/script-writer/json-flattener.py
@@ -10,7 +10,7 @@ import select
 import sys
 
 def args_parser():
-    parser = argparse.ArgumentParser(description='Reads JSON from STDIN and flatten everything from first level children.')
+    parser = argparse.ArgumentParser(description='Reads JSON from STDIN and flatten everything.')
     parser.add_argument('-sep', '--separator', dest='separator', default='.', help='JSON key separator after flattened (default: ".")')
     return parser
 
@@ -29,14 +29,31 @@ def flatten(d, parent_key='', sep='.'):
 
     return dict(items)
 
+def run_tests():
+    in_string = '''{"Data":{"ApiVersion":"1.16","Arch":"amd64","Containers":10,"Debug":true,"DockerRootDir":"/mnt/sda1/var/lib/docker","Driver":{"Dirs":86,"Name":"aufs","RootDir":"/mnt/sda1/var/lib/docker/aufs"},"ExecutionDriver":"native-0.2","GitCommit":"5bc2ff8","GoVersion":"go1.3.3","ID":"A7LJ:M42E:ZPYS:TG37:56A5:NMGP:B7NQ:6UJ5:JUTL:MUXB:Y5E6:6XTI","IPv4Forwarding":true,"Images":58,"IndexServerAddress":"https://index.docker.io/v1/","InitPath":"/usr/local/bin/docker","InitSha1":"","KernelVersion":"3.16.7-tinycore64","Labels":"null","MemTotal":2.106249216e+09,"MemoryLimit":true,"NGoroutines":38,"Name":"boot2docker","NumCPUs":2,"NumEventsListeners":0,"NumFileDescriptors":32,"OperatingSystem":"Boot2Docker 1.4.1 (TCL 5.4); master : 86f7ec8 - Tue Dec 16 23:11:29 UTC 2014","Os":"linux","SwapLimit":true,"Version":"1.4.1"},"GoStruct":"DockerInfoVersion","Host":{"Name":"MacBook-Pro.local","Tags":[]},"Interval":"3s","Path":"/docker/info-version","UnixNano":1430676379672586965}'''
+    in_json = json.loads(in_string)
+    output  = flatten(in_json)
+
+    if in_json["Data"]["Driver"]["Name"] != output["Data.Driver.Name"]:
+        print("Error: Input driver name does not match output driver name.")
+    else:
+        sys.stdout.write('.')
+
+    print("All tests passed.")
+
+
 if __name__ == '__main__':
-    parser = args_parser()
+    if len(sys.argv) == 2:
+        if sys.argv[1] in ["test", "tests"]:
+            run_tests()
+    else:
+        parser = args_parser()
 
-    if not select.select([sys.stdin,],[],[],0.0)[0]:
-        parser.print_help()
-        sys.exit(0)
+        if not select.select([sys.stdin,],[],[],0.0)[0]:
+            parser.print_help()
+            sys.exit(0)
 
-    args = parser.parse_args()
-    output = dict((path, flatten(data, sep=args.separator)) for path, data in read().items())
+        args = parser.parse_args()
+        output = flatten(read(), sep=args.separator)
 
-    print(json.dumps(output))
+        print(json.dumps(output))
