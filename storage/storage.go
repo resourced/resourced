501d567aa6f800ae6a04df6cd5eed5aab7549d9c
Didip Kerabat <didip@newrelic.com>
2015-07-25 22:54:40 -0700

    WIP, metadata storage client side

--- storage/storage.go
+++ storage/storage.go
@@ -1,7 +1,11 @@
 package storage
 
 import (
+	"bytes"
 	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"path"
 	"sync"
 )
 
@@ -34,4 +38,42 @@ func (s *Storage) Get(key string) []byte {
 
 func (s *Storage) ToJson() ([]byte, error) {
 	return json.Marshal(s.Data)
+}
+
+func NewResourcedMasterMetadataStorage(root string) *ResourcedMasterMetadataStorage {
+	s := &ResourcedMasterMetadataStorage{}
+	s.Root = root
+	return s
+}
+
+type ResourcedMasterMetadataStorage struct {
+	Root string
+}
+
+func (s *ResourcedMasterMetadataStorage) Set(key string, data []byte) error {
+	req, err := http.NewRequest("POST", path.Join(s.Root, key), bytes.NewBuffer(data))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.Body != nil {
+		defer resp.Body.Close()
+	}
+
+	return nil
+}
+
+func (s *ResourcedMasterMetadataStorage) Get(key string) ([]byte, error) {
+	resp, err := http.Get(path.Join(s.Root, key))
+	if err != nil {
+		return nil, err
+	}
+	if resp.Body != nil {
+		defer resp.Body.Close()
+	}
+
+	return ioutil.ReadAll(resp.Body)
 }
