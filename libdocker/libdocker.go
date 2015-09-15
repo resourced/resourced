6d0bf919bad4cdac6b84a8f4ee804127e9e8fbe4
Bill Green <bgreen@newrelic.com>
2015-09-15 09:36:04 -0700

    Fix bug with var access within closure
    The gofunc() closure within AllInspectedContainers() is referencing
    shortDescContainer.ID from the enclosing scope. Because
    shortDescContainer.ID is inside of a loop, its value is unpredicable
    while the goroutines are running. The usual effect is that all
    goroutines get the same value for the ID. The solution is to set
    the ID in the larger container before starting the goroutine.
    
    Signed-off-by: Bill Green <bgreen@newrelic.com>

--- libdocker/libdocker.go
+++ libdocker/libdocker.go
@@ -176,6 +176,7 @@ func AllInspectedContainers(endpoint string) ([]*CompleteDockerContainer, error)
 
 	for _, shortDescContainer := range shortDescContainers {
 		container := &CompleteDockerContainer{}
+		container.ID = shortDescContainer.ID
 		container.NiceImageName = shortDescContainer.Image
 		container.Command = shortDescContainer.Command
 		container.Status = shortDescContainer.Status
@@ -185,9 +186,8 @@ func AllInspectedContainers(endpoint string) ([]*CompleteDockerContainer, error)
 		go func(container *CompleteDockerContainer) {
 			defer wg.Done()
 
-			fullDescContainer, err := client.InspectContainer(shortDescContainer.ID)
+			fullDescContainer, err := client.InspectContainer(container.ID)
 			if err == nil && fullDescContainer != nil {
-				container.ID = fullDescContainer.ID
 				container.Created = fullDescContainer.Created
 				container.Path = fullDescContainer.Path
 				container.Args = fullDescContainer.Args
