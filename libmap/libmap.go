998c8b7f27c3257d43cc6f6f69e048d8d74ee33f
Lydia Ngo <lydia.liman@gmail.com>
2016-03-07 07:41:25 -0800

    WIP: NestedMapInterface

--- libmap/libmap.go
+++ libmap/libmap.go
@@ -35,3 +35,44 @@ func (s *TSafeMapBytes) Get(key string) []byte {
 func (s *TSafeMapBytes) ToJson() ([]byte, error) {
 	return json.Marshal(s.Data)
 }
+
+func NewTSafeNestedMapInterface() *TSafeNestedMapInterface {
+	s := &TSafeNestedMapInterface{}
+	s.Data = make(map[string]interface{})
+	return s
+}
+
+type TSafeNestedMapInterface struct {
+	Data map[string]interface{}
+	sync.RWMutex
+}
+
+func (s *TSafeNestedMapInterface) initNestedMap(key string) {
+	// Split key by dot and loop deeper into the nesting & create the maps
+	s.Lock()
+	s.Unlock()
+}
+
+func (s *TSafeNestedMapInterface) Set(key string, value interface{}) {
+	s.initNestedMap(key)
+
+	s.Lock()
+	s.Data[key] = value
+	s.Unlock()
+}
+
+func (s *TSafeNestedMapInterface) Get(key string) interface{} {
+	var data interface{}
+
+	// Split key by dot and loop deeper into the nesting
+
+	s.RLock()
+	data = s.Data[key]
+	s.RUnlock()
+
+	return data
+}
+
+func (s *TSafeNestedMapInterface) ToJson() ([]byte, error) {
+	return json.Marshal(s.Data)
+}
