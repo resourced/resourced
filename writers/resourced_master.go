e6b9600d200808abe4d3432a532856d3b19ce636
Lydia Ngo <lydia.liman@gmail.com>
2016-09-19 21:35:05 -0700

    Flatten data before sending to master.

--- writers/resourced_master.go
+++ writers/resourced_master.go
@@ -1,5 +1,15 @@
 package writers
 
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/sethgrid/pester"
+
+	"github.com/resourced/resourced/libmap"
+)
+
 func init() {
 	Register("ResourcedMasterHost", NewResourcedMasterHost)
 }
@@ -12,4 +22,67 @@ func NewResourcedMasterHost() IWriter {
 // ResourcedMasterHost is a writer that serialize readers data to ResourcedMasterHost.
 type ResourcedMasterHost struct {
 	Http
+}
+
+func (rmh *ResourcedMasterHost) preProcessData() (map[string]interface{}, error) {
+	newData := make(map[string]interface{})
+
+	for readerPath, dataAndHostInterface := range rmh.Data.(map[string]interface{}) {
+		dataAndHost := dataAndHostInterface.(map[string]interface{})
+
+		flatten, err := libmap.Flatten(dataAndHost["Data"].(map[string]interface{}), ".")
+		if err != nil {
+			return nil, err
+		}
+
+		dataAndHost["Data"] = flatten
+		newData[readerPath] = dataAndHost
+	}
+
+	return newData, nil
+}
+
+// Run executes the writer.
+func (rmh *ResourcedMasterHost) Run() error {
+	if rmh.Data == nil {
+		return errors.New("Data field is nil.")
+	}
+
+	flatten, err := rmh.preProcessData()
+	if err != nil {
+		return err
+	}
+
+	dataJson, err := json.Marshal(flatten)
+	if err != nil {
+		return err
+	}
+
+	req, err := rmh.NewHttpRequest(dataJson)
+	if err != nil {
+		return err
+	}
+
+	client := pester.New()
+	client.MaxRetries = int(rmh.MaxRetries)
+	client.Backoff = pester.ExponentialJitterBackoff
+	client.KeepLog = false
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"Error":      err.Error(),
+			"req.URL":    req.URL.String(),
+			"req.Method": req.Method,
+		}).Error("Failed to send HTTP request")
+
+		return err
+	}
+
+	if resp.Body != nil {
+		resp.Body.Close()
+	}
+
+	return nil
 }
